eed2efa0be5bdd3a5253c08e70cbc59f548e1699
ParzivalEugene <michkov.evgeny@gmail.com>
2022-12-08 23:29:40 +0300

    Navbar + Companies

---
+++ src/pages/companies/[id].tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+const data = {
+  id: "some_id1",
+  name: "Skolkovo",
+  tags: ["Technology", "Startup", "Data Science", "AI", "ML", "Software"],
+  websiteURL: "https://sk.ru",
+  logoURL:
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fglobalnetwork.io%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmax_1300x1300%2Fpublic%2F2019-12%2FSKOLKOVO%2520campus.jpg%3Fitok%3DPhFxA6pv&f=1&nofb=1&ipt=f79a35839e5a0deecc77e9fa420f657a656b6e8b140ba99fa289205629e85f8e&ipo=images",
+  contacts: {
+    email: "skolkovo@skolkovo.ru",
+    phone: "8-800-555-35-35",
+    address: "Russia, Moscow, 3rd Tverskaya-Yamskaya street, 15",
+    city: "Moscow",
+    country: "some_country",
+    zip: "some_zip",
+  },
+}
+
+
+function Company() {
+  return (
+    <div>Company</div>
+  )
+}
+
+export default Company
