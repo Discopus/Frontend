3c95090b54c65884da67e34dcc08eaf2e649eeab
Michkov Eugene <michkov.evgeny@gmail.com>
2022-12-15 22:11:20 +0300

    redux fix

---
+++ src/redux/services/UserService.ts
@@ -0,0 +1,40 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import { User, UserForRegistration } from "../models/User";
+import { RootState } from "../store";
+
+export const userAPI = createApi({
+  reducerPath: "userAPI",
+  tagTypes: ["User"],
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:4000/api/users",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set("Authorization", token);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getUsers: builder.query<User[], void>({
+      query: () => ({
+        url: "/getAll",
+      }),
+      providesTags: (result) => ["User"],
+    }),
+    getUserById: builder.query<User, string>({
+      query: (id) => ({
+        url: `/getById/${id}`,
+      }),
+      providesTags: (result) => ["User"],
+    }),
+    addUser: builder.mutation<User, UserForRegistration>({
+      query: (user) => ({
+        method: "POST",
+        url: "/create",
+        body: user,
+      }),
+      invalidatesTags: ["User"],
+    }),
+  }),
+});
